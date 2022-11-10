aa0f4ca9f8a63853e4bbeb22600198f7ee83fe86
Sijmen Aukema <sijmenaukema@gmail.com>
2022-11-10 14:06:52 +0100

    frist commit

---
+++ src/test/java/com/application/festival/FestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.festival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
